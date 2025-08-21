f920660ef0bdda11155e820b2629bd423575c232
Mikel Ribera <a2023115604@estudiantes.upsa.edu.bo>
2025-08-20 21:31:03 -0400

    Add sum endpoints

---
+++ Controllers/CalculatorController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Calculator1.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class CalculatorController: ControllerBase
+{
+    [HttpGet(template: "sum")]
+    public IActionResult GetSum(int a, int b)
+    {
+        var result = a + b;
+        return Ok(new {a, b, result});
+    }
+}
